88cf6a741abb11b7403897bcf7ebe7fe21dd36cd
Matthew Sagen <matthewsagen@Matthews-MacBook-Pro.local>
2018-04-20 08:52:26 -0600

    Initial places API appplication

---
+++ impulse_challenge/Pods/Target Support Files/Pods-impulse_challengeUITests/Pods-impulse_challengeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_impulse_challengeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_impulse_challengeUITests
+@end
---
+++ impulse_challenge/impulse_challenge/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  impulse_challenge
+//
+//  Created by Matthew Sagen on 3/13/18.
+//  Copyright © 2018 Matthew Sagen. All rights reserved.
+//
+
+
+import UIKit
+import GooglePlaces
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var selectionlabel: UILabel!
+    // Present the Autocomplete view controller when the button is pressed.
+    @IBAction func autocompleteClicked(_ sender: UIButton) {
+        let autocompleteController = GMSAutocompleteViewController()
+        autocompleteController.delegate = self
+        present(autocompleteController, animated: true, completion: nil)
+    }
+    
+}
+
+extension ViewController: GMSAutocompleteViewControllerDelegate {
+    
+    // Handle the user's selection.
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        print("Place name: \(place.name)")
+        print("Place address: \(place.formattedAddress)")
+        print("Place attributions: \(place.attributions)")
+        dismiss(animated: true, completion: nil)
+        var userSelection = place.name
+        selectionlabel.text = userSelection
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        // TODO: handle the error.
+        print("Error: ", error.localizedDescription)
+    }
+    
+    // User canceled the operation.
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // Turn the network activity indicator on and off again.
+    func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+}
+
